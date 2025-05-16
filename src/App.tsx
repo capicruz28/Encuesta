af6bcb2955757434334283ce90667c491b059274
carpin <carpin@CARPIN>
2025-05-16 14:41:00 -0500

    app encuesta quitar mantenimiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Login from './pages/Login';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import DashboardPage from './pages/DashboardPage';
 import Sectores from './pages/Sectores';
@@ -8,22 +8,22 @@ import Encuestas from './pages/Encuestas';
 import Preguntas from './pages/Preguntas';
 import LlenarEncuesta from './pages/LlenarEncuesta';
 import Reportes from './pages/Reportes';
-//import ResponderEncuesta from './pages/ResponderEncuesta'; // Importamos el nuevo componente
+import ResponderEncuesta from './pages/ResponderEncuesta'; // Importamos el nuevo componente
 import GeneradorQR from './pages/GenerarQR';
 import Secciones from './pages/Secciones';
-import EnMantenimiento from './pages/EnMantenimiento';
+//import EnMantenimiento from './pages/EnMantenimiento';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Ruta pública para responder encuestas */}
-        <Route path="/responder-encuesta/:id" element={<EnMantenimiento />} />
-        {/*<Route path="/responder-encuesta/:id" element={<ResponderEncuesta />} />*/}
+        {/*<Route path="/responder-encuesta/:id" element={<EnMantenimiento />} />*/}
+        <Route path="/responder-encuesta/:id" element={<ResponderEncuesta />} />
         
         {/* Rutas existentes */}
-        <Route path="/" element={<EnMantenimiento />} />
-        {/*<Route path="/" element={<Login />} />*/}
+        {/*<Route path="/" element={<EnMantenimiento />} />*/}
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />}>
           <Route index element={<DashboardPage />} />
           <Route path="sectores" element={<Sectores />} />
